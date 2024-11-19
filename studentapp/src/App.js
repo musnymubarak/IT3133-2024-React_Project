3929b2cd76bbe224e505fbf6d61d0d1e8d9ef8b1
Musny Mubarak <119862882+musnymubarak@users.noreply.github.com>
2024-11-19 08:46:22 +0000

    StudentTable component is created

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React, { useState } from 'react';
-import './App.css';
+import './Assets/css/compo.css'
 
 function App() {
   const [fontSize, setFontSize] = useState('medium'); 
@@ -12,12 +12,19 @@ function App() {
   return (
     <div className={`app-container font-${fontSize}`} style={{ fontSize: fontSize === 'small' ? '16px' : fontSize === 'medium' ? '20px' : '24px' }}>
       <h1>Students Information Portal</h1>
+      <hr/>
       <div className="font-size-controls">
         <span>Font Size: </span>
         <button onClick={() => handleFontSizeChange('small')}>S</button>
         <button onClick={() => handleFontSizeChange('medium')}>M</button>
         <button onClick={() => handleFontSizeChange('large')}>L</button>
       </div>
+
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
     </div>
   );
 }
